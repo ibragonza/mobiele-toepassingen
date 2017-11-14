58915a516b5770afcb93d4addeb0192150777592
Lennert Wieërs <lennert.wieers@gmail.com>
2017-11-14 11:07:41 +0100

    Change to be able to pull

--- Karavaan8/JSONTransformer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StyleSheet, Text, View,Button,Alert,TouchableHighlight, Component} from 'react-native';
-const util = require("util");
-
-export function Test()
-{
-	Alert.alert("Hallo")
-}
