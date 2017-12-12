8cdef4625a611ab5201f8ec98d2f7d62d539a637
joris <joris.vankemmel@skynet.be>
2017-12-12 11:24:47 +0100

    Add Expense

--- Karavaan8/screens/AddExpense.js
+++ Karavaan8/screens/AddExpense.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import { StyleSheet, Text, View, Alert, TouchableHighlight, Image, Dropdown, TextInput } from 'react-native';
 import OurPicker from '../view/OurPicker.js';
+import DatePicker from 'react-native-datepicker'
 
 
 const util = require("util");
 
 export default class AddExpense extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state= {myNumber : '0'};
+        this.state = { myNumber: '0', start_date: "", end_date: "" };
     }
     render() {
         return (
             <Image source={require('../images/expense-background.png')} style={styles.container}>
                 <View style={styles.navbar}>
                     <Text style={styles.header}>Add an Expense</Text>
-                </View>
-                <View style={styles.navbar}>
+                    <Text style={styles.entryText}>Link to trip</Text>
+                    <OurPicker values={["Barcelona", "Tremelo", "Moscow"]} defaultVal={"Tremelo"} />
+                    <Text style={styles.entryText}>Amount</Text>
                     <TextInput
                         style={styles.textInput}
-                        editable = {true}
+                        keyboardType='numeric'
+                        editable={true}
                     />
+                    <OurPicker values={["USD", "YEN", "EURO"]} defaultVal={"EURO"} />
                     <Text style={styles.entryText}>Category</Text>
-                    <OurPicker values={["Option 1", "Option 2"]} />
+                    <OurPicker values={["Option 1", "Option 2"]} defaultVal={"Option 1"} />
+                    <Text style={styles.entryText}>Description</Text>
+                    <TextInput
+                        style={styles.textInput}
+                        keyboardType='numeric'
+                        editable={true}
+                    />
+                    <Text style={styles.entryText}>Date</Text>
+                    <DatePicker style={styles.date} date={this.state.start_date} mode="date" placeholder="select date" format="DD-MM-YYYY" confirmBtnText="Confirm" cancelBtnText="Cancel"
+                        onDateChange={(date) => { this.setState({ start_date: date }) }} customStyles={{ dateText: { color: 'black', }, placeholderText: { color: 'black', }, }} />
                 </View>
+                <TouchableHighlight style={styles.addExpensebutton} onPress={() => navigate("AddExpense", {})}>
+                    <View>
+                        <Text style={styles.buttonText}>ADD EXPENSE</Text>
+                    </View>
+                </TouchableHighlight>
             </Image>
         );
     }
@@ -35,7 +53,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold'
     },
     entryText: {
-        fontSize: 36
+        fontSize: 24
     },
     navbar: {
         flex: 1,
@@ -53,7 +71,23 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    textInput:{
-        backgroundColor:"red"
-    }
+    addExpensebutton:
+        {
+            marginTop: 5,
+            marginBottom: 10,
+            borderRadius: 3,
+            width: 200,
+            height: 70,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            backgroundColor: '#00FF7F',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+    buttonText:
+        {
+            fontSize: 20,
+            color: 'white',
+            textAlign: 'center',
+        }
 });
